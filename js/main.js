ced1b48aa86ef2ee193484b1f3343a8df94b72c8
KIANONI <coffeebee777@gmail.com>
2022-05-15 17:02:50 +0300

    main

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        items: 2,
+        dots: true,
+        margin: 92,
+        smartSpeed: 500,
+    });
+  });
